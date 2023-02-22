68bfc26af0d69769006f9daac9f9fa65d3c30f22
myUsername <myemail@gmail.com>
2023-02-22 13:05:44 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+
+
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function that prints all natural numbers to 98
+ * @n: the number that the function is going to start from is passed here.
+ */
+void print_to_98(int n)
+{    
+int i;
+if (n < 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
+printf("%d, ", i);
+}
+printf("\n");
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
+printf("%d, ", i);
+}
+printf("\n");
+}
+}
